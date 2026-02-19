a0982b0a26e5f5d365e7fedc76ab5c89c80793f5
alxjr.wav <20245387@esen.edu.sv>
2026-02-19 07:26:43 -0600

    chore: routes definidos

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,25 @@ use Illuminate\Support\Facades\Route;
 
 // Endpoints:
 // TODO: GET /books
+Route::get( // Lista
+    'books', 
+    [BookController::class, 'index']
+);
+
 // TODO: filtros: titulo, isbn, status
+Route::get( // Obtener 
+    'books/{book}', // puede ser isbn, titulo o status
+    [BookController::class, 'show']
+);
 
 // TODO: POST /loans
+Route::post( // Crear loan
+    'loans', // detallar en JSON: nombre de solicitante, fecha/hora y libro
+    [LoanController::class, 'store']
+); // Si no hay existencias, retornar error 422
 
 // TODO: POST /returns/{loan_id}
-
+Route::post( // crear return
+    'returns/{loan_id}',
+    [LoanController::class, 'store']
+);
